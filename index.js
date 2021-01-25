ebe91c6f99d818ef5e22c5c59f4d8f4c1c5967eb
okazkayasi <mkazkayasi3@gatech.edu>
2021-01-25 22:19:54 +0300

    node sending message

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const koa = require("koa");
 const bodyParser = require("koa-bodyparser");
+const fetch = require("node-fetch");
 
 const koaRouter = require("koa-router");
 const app = new koa();
 app.use(bodyParser());
 
 const router = new koaRouter();
+
+const sendMessage = async (message) => {
+  fetch(
+    "https://hooks.slack.com/services/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",
+    { method: "POST", body: JSON.stringify(message) }
+  )
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err, "err"));
+};
 
 router.get("/", (ctx) => {
   ctx.body = "Hello World!";
@@ -17,17 +27,22 @@ router.post("/formdata", (ctx) => {
   console.log(body, "formdata");
   const message = `Contact form is submitted by a new person:
 ${JSON.stringify(body).slice(1, -1).split(",").join("\n")}`;
-
   // this message is to be sent
   console.log(message);
+  const mes = { text: message };
+  sendMessage(mes);
+  ctx.body = "Ok";
 });
 
 router.post("/journeyData", (ctx) => {
   const body = ctx.request.body;
   // here we go
-  const message = `Contact form is submitted by a new person:
+  const message = `Journey is completed by a new person:
 ${JSON.stringify(body).slice(1, -1).split(",").join("\n")}`;
   console.log(message);
+  const mes = { text: message };
+  sendMessage(mes);
+  ctx.body = "Ok";
 });
 
 app.use(router.routes()).use(router.allowedMethods());
